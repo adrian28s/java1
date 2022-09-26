0da3af72989b6ba9853190f6ad096a6883095cf5
adrian28s <addrians28@gmail.com>
2022-09-26 21:40:52 +0200

    mi primer js

---
+++ main.js
@@ -0,0 +1,230 @@
+
+//1 VARIABLES
+
+//1
+// alert("JavaScript funcionando correctamente")
+//2
+// Para llamar al cradro de dialo se usa //
+/* para llamar al comentario de bloque se usa /*  */
+
+//3
+let variableSinValor = ""
+
+//4
+let booleano1 = true
+let booleano2 = false
+console.log(booleano1)
+console.log(booleano2)
+
+//5
+const PI = 3.14
+console.log(PI)
+
+//6
+const TAU = PI * 2
+console.log(TAU)
+
+//7
+const miNombre = "adrian"
+console.log(miNombre)
+
+//8
+const miNumeroFav = 2
+console.log(miNumeroFav)
+
+//9
+const verdadero = true
+
+//10
+console.log(miNombre)
+
+//11
+console.log(miNumeroFav)
+
+//12
+console.log("Mi nombre es " + miNombre + " y mi numero favorito es " + miNumeroFav)
+
+//13
+const text = "Seré un crack en JavaScript al terminar el bootcamp"
+let val;
+val = text.toUpperCase();
+console.log(val)
+
+//14
+const text1 = "Hola soy un crack"
+val = text1.substring(0, 5);
+console.log(val)
+
+//15
+val = miNumeroFav
+console.log(val)
+
+//16
+
+//17
+console.log(PI.toFixed(2))
+
+//ARRAYS
+//1
+const arrayVacio = ""
+
+//2
+const arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(arrayNumeros)
+
+//3
+const arrayNumeros2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+console.log(arrayNumeros2)
+
+//4
+const arrayNumerosPares = [0, 2, 4, 6, 8, 10]
+console.log(arrayNumerosPares)
+
+//5
+const arrayNumerosNeg = [0, -1, -2, -3, -4, -5, -6, -7, -8, -9]
+
+//6
+const holaMundo = ["hola", "mundo"]
+
+//7
+const loGuardoTodo = ["hola", "que", "23", "42.33", "tal"]
+
+//8
+const arrayDeArrasys = [["756", "nombre"], ["225", "apellido"], ["298", "direccion"]]
+
+//9
+console.log(loGuardoTodo.length)
+
+//10
+const nuevoElemento = "NuevoElemento"
+console.log(loGuardoTodo + nuevoElemento)
+
+//11
+console.log(loGuardoTodo)
+
+//12
+
+
+//OBJETOS
+
+
+//1
+const coche = {
+    marca: "opel",
+    modelo: "astra",
+    matricula: "1149GRZ"
+    }
+console.log(coche)
+
+//2
+const casa = {
+    codPostal: "46380",
+    calle: "Mendes nuñez",
+    numero: 28
+}
+console.log(casa)
+
+//3
+const FuulStackDeveloper= {
+    leguajes: ["javascript", "php"],
+    proyectos: "mi paguína personal"
+}
+console.log(FuulStackDeveloper)
+
+//4
+const perro = {
+    nombre: "oddy",
+    raza: "yorshire",
+    color: "negro",
+    edad: "10 años"
+}
+console.log(perro)
+
+//5
+const noticia = {
+    titular: "Mi primer js",
+    noticia: "noticias varias"
+}
+console.log(noticia)
+
+//6
+const person = {
+    nombre: "adrian",
+    apellido: "sanchez",
+    edad: 28
+}
+console.log(person)
+
+//8
+console.log(person.nombre)
+
+//9
+console.log(FuulStackDeveloper.leguajes[0])
+
+//10
+const portatil = {
+    marca: "asus"
+}
+console.log(portatil.marca)
+
+//12
+
+//13
+const concierto = {
+    grupos: ["la oreja de vang go"]
+}
+console.log(concierto.grupos)
+
+//14
+const led = {
+    array: ["rojo", "verde", "azul"]
+}
+console.log(led.array)
+
+//15
+
+//16
+const grupo = {
+    integrante: ["pedro", "pablo", "adrian"]
+}
+console.log(grupo.integrante[2])
+
+//17
+const impresora = {
+    tinta: ["rojo", "verde", "azul"]
+}
+console.log(impresora.tinta)
+
+//18
+const movil = {
+    especificaciones: ["especificaciones"]
+}
+console.log(["especificaciones"])
+
+//19
+const portatil1 = {
+    marca: "MSI"
+}
+console.log(portatil1.marca)
+
+//20 no me salia ni con el ejemplo de clase, los marco para que no de error
+//const numbersAndFruits = ['apples',10, 'oranges', 8,9,true]
+//console.log(numbersAndFruits.push("manzana"))
+
+//21como no enetendi el 20 ya no pude hacer el 21 ni 22
+
+//22
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
